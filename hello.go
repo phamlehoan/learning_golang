c81e438e86cafa17bee263c1bb182a81bdf99a5a
phamlehoan <phamlehoan@github.com>
2022-11-05 12:02:52 +0700

    Middleware

--- hello.go
+++ hello.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"time"
+
 	"github.com/kataras/iris/v12"
 )
 
+func myMiddleware(ctx iris.Context) {
+	ctx.Application().Logger().Infof("Thời gian nhận request: %s", time.Now())
+	ctx.Next()
+}
+
 func main() {
 	app := iris.New()
+	app.Use(myMiddleware)
 	app.Handle("GET", "/", func(ctx iris.Context) {
 		ctx.HTML("Hello world")
 	})
